1bc8cbaf2450ddb14531e90c7132242f571099ea
hritu567 <hritumog96@gmail.com>
2020-11-24 13:53:27 +0530

    Added : stack 100 for every day and bet 1 for every game

---
+++ gambling.js
@@ -0,0 +1,4 @@
+console.log("Welcome to Gambling simulation problem");
+const utility = require('./utility');
+var Utility= require('./utility');
+utility.gamblingSimulator();
--- utility.js
+++ utility.js
@@ -1,1 +1,9 @@
-console.log("Welcome to Gambling simulation problem");
+//uc1
+//stack 100 for every day and bet 1 for every game
+function gamblingSimulator()
+{
+    let stack=100;
+    let bet=1;
+    console.log("stack is : " + stack +" and bet is : "+ bet +" for every game");
+}
+module.exports={gamblingSimulator};
