1609f48297c17d3b2e972be9fee99709513d02e3
hritu567 <hritumog96@gmail.com>
2020-11-24 19:58:01 +0530

    Added : total winning or losing amount after 20 days

--- utility.js
+++ utility.js
@@ -1,22 +1,30 @@
-//uc3
-//If in or loose 50% of stack the gambler will resign
+//uc4
+//After 20 days of playing total amount win or loss
 function gamblingSimulator()
 {
     const stack=100;
     const bet=1;
-    const WIN_CASH=stack+stack/2;
-    const LOST_CASH=stack-stack/2;
-    console.log("stack is : " + stack +" and bet is : "+ bet +" for every game");
-    let isWin=1;
+    const isWin=1;
+    const daysInMonth=20;
+    let maxWin=stack+stack/2;
+    let maxLoose=stack-stack/2;
     let cash=stack;
+    let totalcash=0;
+    for(index=1;index<=daysInMonth;index++)
+    {
+    cash=stack;
+    while(cash != maxWin && cash != maxLoose){
     let ran=Math.floor(Math.random()*10)%2;
-    while(cash != WIN_CASH && cash != LOST_CASH){
     if(ran==isWin)
     
        cash++;
     else
         cash--;
   }
-  console.log("cash is : "+cash);
+  cash=cash-stack;
+  totalcash=totalcash+cash;
+  console.log("cash is : "+(cash-stack));
+ }
+console.log("total cash : "+totalcash);
 }
 module.exports={gamblingSimulator};
